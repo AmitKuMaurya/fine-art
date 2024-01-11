ece5ff8776e8a1c0f59f4c8696fdc1616d176b6e
[name] <jaipal@createbytes.com>
2024-01-11 15:22:34 +0530

    exoprted all constant or mock data to homepage.constant.js and removed all extra line of code

---
+++ src/constants/homepage.constant.js
@@ -0,0 +1,93 @@
+// Seocnd floor images
+import Rectangle18 from "../assets/second_section_img/Rectangle18.png";
+import Rectangle19 from "../assets/second_section_img/Rectangle19.png";
+import Rectangle20 from "../assets/second_section_img/Rectangle20.png";
+import Rectangle21 from "../assets/second_section_img/Rectangle21.png";
+import Rectangle44 from "../assets/second_section_img/Rectangle44.png";
+import Rectangle45 from "../assets/second_section_img/Rectangle45.png";
+import Rectangle46 from "../assets/second_section_img/Rectangle46.png";
+import Rectangle47 from "../assets/second_section_img/Rectangle47.png";
+import Rectangle48 from "../assets/second_section_img/Rectangle48.png";
+import Rectangle49 from "../assets/second_section_img/Rectangle49.png";
+
+// images for third floor
+
+import Theme107 from "../assets/third_section_imgs/Frame107.png";
+import Theme108 from "../assets/third_section_imgs/Frame108.png";
+import Theme109 from "../assets/third_section_imgs/Frame109.png";
+import Theme110 from "../assets/third_section_imgs/Frame110.png";
+
+// images for fourth floor
+import Vector0 from "../assets/fourth_floor/Layer1.png";
+import Vector1 from "../assets/fourth_floor/Layer2.png";
+import Vector2 from "../assets/fourth_floor/Layer3.png";
+import Vector3 from "../assets/fourth_floor/layer4.png";
+
+// fifth floor
+import Rectangle80 from "../assets/fifthFloor/Rectangle80.png";
+
+export const imgForFithFloor = Rectangle80;
+// sixth floor Content
+import Profile1 from "../assets/sixth_section/Profile1.png";
+import Profile2 from "../assets/sixth_section/Profile2.png";
+import Profile3 from "../assets/sixth_section/Profile3.png";
+
+// seventh floor Content.
+import Theme1 from "../assets/seventh_section/Frame112.png";
+import Theme2 from "../assets/seventh_section/Frame113.png";
+import Theme3 from "../assets/seventh_section/Frame114.png";
+import Theme4 from "../assets/seventh_section/Frame115.png";
+import Theme5 from "../assets/seventh_section/Frame116.png";
+
+export const images = [
+    Rectangle18,
+    Rectangle19,
+    Rectangle20,
+    Rectangle21,
+    Rectangle44,
+    Rectangle45,
+    Rectangle46,
+    Rectangle47,
+    Rectangle48,
+    Rectangle49,
+  ];
+
+export const thirdSectionImages = [
+    { img: Theme107, name: "ABC Category 2023", quant: 28 },
+    { img: Theme108, name: "ABC Category 2023", quant: 71 },
+    { img: Theme109, name: "ABC Category 2023", quant: 57 },
+    { img: Theme110, name: "ABC Category 2023", quant: 48 },
+  ];
+
+ export const imagesForFourth = [
+    { img: Vector0, desc: "Complete Packages For All Your Designs" },
+    { img: Vector1, desc: "Over 10 Years Of Experience" },
+    { img: Vector2, desc: "Expert Guides For You" },
+    { img: Vector3, desc: "Guaranteed quality at the best price!" },
+  ];
+
+ export const sixthFloorItem = [
+    {
+      img: Profile1,
+      name: "Rohit Sain",
+      desc: "ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+    },
+    {
+      img: Profile2,
+      name: "Maya Singh",
+      desc: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet",
+    },
+    {
+      img: Profile3,
+      name: "Suraj Kumar",
+      desc: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+    },
+  ];
+
+ export const SeventhFloorImgs = [
+    {img : Theme5},
+    {img : Theme1},
+    {img : Theme2},
+    {img : Theme3},
+    {img : Theme4}
+  ];
