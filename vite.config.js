535d87bd969550b2a5385bb31fecec3dfe99b6c5
[name] <jaipal@createbytes.com>
2024-01-12 17:23:46 +0530

    added vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/",
+  base : "./index.html",
   plugins: [react()],
   build: {
     rollupOptions: {
