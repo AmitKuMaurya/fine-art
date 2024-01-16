03b3e0edfb56b9ed116596237a3322472e314abd
[name] <amit69maurya69@gmail.com>
2024-01-16 17:24:35 +0530

    created a branch to test and deploy code to vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 import {Outlet} from "react-router-dom";
-
 function App() {
 
   return (
