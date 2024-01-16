66b0681042a0886f3808c8ceae29871b64fa9ba2
[name] <jaipal@createbytes.com>
2024-01-16 16:55:23 +0530

    removed AllRouter file and modified with mordern standard of routing with outlet on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
-import { AllRoutes } from './components/routes/AllRoutes'
-// import { HomePage } from './components/home/HomePage';
-
+import {Outlet} from "react-router-dom";
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <AllRoutes />
+      <Outlet/>
       <Footer/>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,70 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter ,RouterProvider } from 'react-router-dom';
+import { AboutUs } from './components/about_us/AboutUs.jsx';
+import { Login } from './components/login/Login.jsx';
+import SignUp from './components/sign_up/SignUp.jsx';
+import { ContactUs } from './components/contact_us/ContactUs.jsx';
+import { Cart } from './components/cart/Cart.jsx';
+import { Product } from './components/product/Product.jsx';
+import { PrivacyPolicy } from './components/privacy_policy/PrivacyPolicy.jsx';
+import { Category } from './components/category/Category.jsx';
+import { Pricing } from './components/pricing/Pricing.jsx';
+import { HomePage } from './components/home/HomePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <App/>,
+    children : [
+      {
+        path : "/",
+        element : <HomePage/>
+      },
+      {
+        path : "/log-in",
+        element : <Login/>
+      },
+      {
+        path : "/sign-up",
+        element : <SignUp/>
+      },
+      {
+        path : "/contact-us",
+        element : <ContactUs/>
+      },
+      {
+        path : "/about-us",
+        element : <AboutUs/>
+      },
+      {
+        path : "/cart",
+        element : <Cart/>
+      },
+      {
+        path : "/category",
+        element : <Category/>
+      },
+      {
+        path : "/product",
+        element : <Product/>
+      },
+      {
+        path : "/pricing",
+        element : <Pricing/>
+      },
+      {
+        path : "/privacy-policy",
+        element : <PrivacyPolicy/>
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
